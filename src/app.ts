b16eb82e41e15b4682982589b2123fbe412df442
Bohdlesk <bohdanleskovets@gmail.com>
2021-03-28 18:36:24 +0300

    Added a call to the synchronization function with the database

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import { connectToDataBase } from './db';
 
 export const app = express();
 
 app.use(express.json());
+
+// connection to data base
+connectToDataBase();
