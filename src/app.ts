bfe35c9ad65f5fc2537c13512784b3d1634ac203
Bohdlesk <bohdanleskovets@gmail.com>
2021-04-05 22:38:21 +0300

    router connected. db Synchronize and fill added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,220 @@
 import express from 'express';
 import { connectToDataBase } from './db';
+import Cashier, { Sex } from './models/cashier';
+import Supermarket from './models/supermarket';
+import Schedule, { Days, Shift } from './models/schedule';
+import { apiRouterV1 } from './router';
 
 export const app = express();
 
 app.use(express.json());
 
-// connection to data base
-connectToDataBase();
+app.use('/api/v1', apiRouterV1);
+
+async function connectAndSynchronize() {
+  await connectToDataBase();
+
+  await Supermarket.sync({ force: true });
+  await Cashier.sync({ force: true });
+  await Schedule.sync({ force: true });
+
+  await Supermarket.create({
+    address: 'Шевченка 100',
+    city: 'Київ',
+    Cashiers: [
+      {
+        first_name: 'Max',
+        last_name: 'Pain',
+        sex: Sex.male,
+        age: 20,
+        work_experience: 8,
+        previous_job: 'Сільпо',
+      },
+      {
+        first_name: 'Max',
+        last_name: 'Pain',
+        sex: Sex.male,
+        age: 20,
+        work_experience: 1,
+        previous_job: 'Сільпо',
+      },
+      {
+        first_name: 'Max',
+        last_name: 'Pain',
+        sex: Sex.male,
+        age: 20,
+        work_experience: 8,
+        previous_job: 'Арсен',
+      }, {
+        first_name: 'Max',
+        last_name: 'Pain',
+        sex: Sex.male,
+        age: 20,
+        work_experience: 2,
+        previous_job: 'Арсен',
+      },
+      {
+        first_name: 'Max',
+        last_name: 'Pain',
+        sex: Sex.male,
+        age: 20,
+        work_experience: 1,
+        previous_job: 'Сільпо',
+      },
+      {
+        first_name: 'Billi',
+        last_name: 'Last',
+        sex: Sex.male,
+        age: 20,
+        work_experience: 6,
+        previous_job: 'Сільпо',
+        Schedules: [
+          {
+            cashbox_number: 2,
+            day: Days.monday,
+            shift: Shift.night,
+          },
+          {
+            cashbox_number: 1,
+            day: Days.friday,
+            shift: Shift.day,
+          },
+        ],
+      },
+      {
+        first_name: 'Max',
+        last_name: 'Pain',
+        sex: Sex.male,
+        age: 27,
+        work_experience: 1,
+        previous_job: 'Сільпо',
+        Schedules: [
+          {
+            cashbox_number: 1,
+            day: Days.friday,
+            shift: Shift.night,
+          },
+        ],
+      },
+      {
+        first_name: 'Max',
+        last_name: 'Pain',
+        sex: Sex.male,
+        age: 27,
+        work_experience: 1,
+        previous_job: 'Сільпо',
+        Schedules: [
+          {
+            cashbox_number: 1,
+            day: Days.friday,
+            shift: Shift.day,
+          },
+        ],
+      },
+    ],
+  }, {
+    include: [
+      {
+        association: Supermarket.Cashier,
+        include: [Cashier.Schedule],
+      },
+    ],
+  });
+
+  await Supermarket.create({
+    address: 'Шевченка 100',
+    city: 'Львів',
+    Cashiers: [
+      {
+        first_name: 'Max',
+        last_name: 'Pain',
+        sex: Sex.male,
+        age: 20,
+        work_experience: 8,
+        previous_job: 'Сільпо',
+      },
+      {
+        first_name: 'Max',
+        last_name: 'Pain',
+        sex: Sex.male,
+        age: 20,
+        work_experience: 1,
+        previous_job: 'Сільпо',
+      },
+      {
+        first_name: 'Max',
+        last_name: 'Pain',
+        sex: Sex.male,
+        age: 20,
+        work_experience: 8,
+        previous_job: 'Арсен',
+      }, {
+        first_name: 'Max',
+        last_name: 'Pain',
+        sex: Sex.male,
+        age: 20,
+        work_experience: 2,
+        previous_job: 'Арсен',
+      },
+      {
+        first_name: 'Billi',
+        last_name: 'Last',
+        sex: Sex.male,
+        age: 20,
+        work_experience: 6,
+        previous_job: 'Сільпо',
+        Schedules: [
+          {
+            cashbox_number: 1,
+            day: Days.monday,
+            shift: Shift.night,
+          },
+          {
+            cashbox_number: 1,
+            day: Days.friday,
+            shift: Shift.day,
+          },
+        ],
+      },
+      {
+        first_name: 'Max',
+        last_name: 'Pain',
+        sex: Sex.male,
+        age: 27,
+        work_experience: 1,
+        previous_job: 'Сільпо',
+        Schedules: [
+          {
+            cashbox_number: 1,
+            day: Days.monday,
+            shift: Shift.night,
+          },
+        ],
+      },
+      {
+        first_name: 'Max',
+        last_name: 'Pain',
+        sex: Sex.male,
+        age: 27,
+        work_experience: 1,
+        previous_job: 'Сільпо',
+        Schedules: [
+          {
+            cashbox_number: 1,
+            day: Days.friday,
+            shift: Shift.day,
+          },
+        ],
+      },
+    ],
+  }, {
+    include: [
+      {
+        association: Supermarket.Cashier,
+        include: [Cashier.Schedule],
+      },
+    ],
+  });
+}
+
+connectAndSynchronize();
