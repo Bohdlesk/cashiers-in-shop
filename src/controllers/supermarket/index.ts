2112384d2e465c876afa0d8af2deb1670952978e
Bohdlesk <bohdanleskovets@gmail.com>
2021-04-05 21:28:01 +0300

    lint fix

--- src/controllers/supermarket/index.ts
+++ src/controllers/supermarket/index.ts
@@ -7,15 +7,3 @@ import deleteSupermarket from './deleteSupermarket';
 export {
   createSupermarket, getSupermarketById, getSupermarketsList, editSupermarket, deleteSupermarket,
 };
-
-// import { Request, Response } from 'express';
-//
-// export default async (req: Request, res: Response): Promise<void> => {
-//   try {
-//
-//   } catch (error) {
-//     res.status(500).json({
-//       error,
-//     });
-//   }
-// };
