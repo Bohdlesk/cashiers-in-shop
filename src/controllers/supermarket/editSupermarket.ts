d1b30884ce480e396f6273c67eddec7a1b04102f
Bohdlesk <bohdanleskovets@gmail.com>
2021-04-05 21:06:53 +0300

    edit supermarket controller added

---
+++ src/controllers/supermarket/editSupermarket.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import Supermarket from '../../models/supermarket';
+
+export default async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const supermarket = await Supermarket.update(
+      req.body,
+      {
+        where: { id },
+        returning: true,
+      },
+    );
+
+    if (!supermarket[0]) {
+      res.status(404).json({
+        message: `Supermarket #${id} is not found`,
+      });
+    } else {
+      res.status(200).json({
+        supermarket: supermarket[1],
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error,
+    });
+  }
+};
