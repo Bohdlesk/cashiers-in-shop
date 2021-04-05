0beee03345ba7adecda23bdc398502fb25beb8e9
Bohdlesk <bohdanleskovets@gmail.com>
2021-04-05 21:26:42 +0300

    cashier router update

--- src/router/cashier.ts
+++ src/router/cashier.ts
@@ -4,4 +4,7 @@ import * as controller from '../controllers/cashier';
 export const cashiersRouter = Router({ mergeParams: true });
 
 cashiersRouter.post('/', controller.createCashier);
-cashiersRouter.delete('/:id', controller.createCashier);
+cashiersRouter.delete('/:id', controller.deleteCashier);
+cashiersRouter.put('/:id', controller.editCashierInfo);
+cashiersRouter.get('/:id', controller.getCashierById);
+cashiersRouter.get('/', controller.getCashiersList);
