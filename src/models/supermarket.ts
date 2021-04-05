e59347af0d9a8249f6db675972819fd59af5d1a7
Bohdlesk <bohdanleskovets@gmail.com>
2021-04-05 12:40:53 +0300

    Supermarket db model update

--- src/models/supermarket.ts
+++ src/models/supermarket.ts
@@ -1,16 +1,22 @@
 /* eslint-disable camelcase */
-import { Model, DataTypes } from 'sequelize';
+import { Model, DataTypes, HasMany } from 'sequelize';
 import { db } from '../db';
+import Cashier from './cashier';
 
 interface ISupermarket {
-  id: number
+  id?: number
   address: string
+  city: string
 }
 
 export default class Supermarket extends Model implements ISupermarket {
   public id!: number;
 
   public address!: string;
+
+  public city!: string;
+
+  static Cashier: HasMany<Supermarket, Cashier>;
 }
 
 Supermarket.init({
@@ -24,8 +30,15 @@ Supermarket.init({
     type: DataTypes.STRING,
     allowNull: false,
   },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 }, {
   sequelize: db,
   tableName: 'supermarkets',
   timestamps: false,
+  underscored: true,
 });
+
+Supermarket.Cashier = Supermarket.hasMany(Cashier, { foreignKey: 'supermarket_id' });
