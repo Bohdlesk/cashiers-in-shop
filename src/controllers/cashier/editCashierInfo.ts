83f3fcded15074fc4b274a22140ce9fae8bd6b7d
Bohdlesk <bohdanleskovets@gmail.com>
2021-04-05 12:46:48 +0300

    Edit Cashier controller added

---
+++ src/controllers/cashier/editCashierInfo.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+import { UpdateOptions } from 'sequelize';
+import Cashier from '../../models/cashier';
+
+export default async (req: Request, res: Response): Promise<void> => {
+  try {
+    const updateOptions: UpdateOptions = {
+      where: {
+        ...req.params,
+      },
+    };
+    const cashier = await Cashier.update(
+      req.body,
+      updateOptions,
+    );
+
+    if (!cashier[0]) {
+      res.status(404).json({
+        message: `Cashier #${req.params.id} is not found`,
+      });
+    } else {
+      res.status(200).json({
+        cashier: cashier[1],
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error,
+    });
+  }
+};
